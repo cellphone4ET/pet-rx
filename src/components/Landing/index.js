14e6bdf4a7c3e3973434cd35c0b4fd2717e39936
Erica Johnson <ericacjohnson@gmail.com>
2018-09-13 16:13:36 -0700

    landing page minus buttons at bottom

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,33 +1,96 @@
 import React from "react";
 // import { connect } from "react-redux";
-// import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar from "./navbar";
 import "./index.css";
 import "./navbar.css";
+import "./float-grid.css";
 
 export function LandingPage(props) {
   return (
-
     <div className="home">
-    	<div className="hero">
-      	<h1 className="landingHeader">Pet.rx</h1>
+      <div className="hero">
+        <h1 className="landingHeader">Pet.rx</h1>
       </div>
 
       <Navbar />
 
-      <div className="highlights content">
-      	<div className="section">
-      		<h2>When was the last time Fido went to the vet?</h2>
-      		<p>Can’t remember the last time your pet had their rabies vaccination?  How about the last time they went to the vet? </p>
-      	</div>
-      	<div className="section">
-      		<h2>Responsible products</h2>
-      		<p>Affogato man bun iPhone poke you probably havent heard of them beard keffiyeh street art kinfolk raw denim retro aesthetic lo-fi.</p>
-      	</div>
-      	<div className="section">
-      		<h2>Doing our part</h2>
-      		<p>Affogato man bun iPhone poke you probably havent heard of them beard keffiyeh street art kinfolk raw denim retro aesthetic lo-fi.</p>
-      	</div>
+      <div className="threebasics content">
+        <div id="main-statement">
+          <p>
+            <span id="main-statment-span">
+              Take control of your pets health with
+              <br />
+            </span>
+            <div id="petrx">Pet.rx</div>
+          </p>
+          <img
+            className="pawimage"
+            src="https://image.flaticon.com/icons/svg/87/87979.svg"
+            alt="paw prints"
+          />
+        </div>
+
+        <hr id="firstline" />
+
+        <div className="col-4">
+          <div className="card">
+            <img
+              className="card-image"
+              src="https://image.flaticon.com/icons/svg/88/88007.svg"
+              alt="vaccine icon"
+            />
+            <div className="card-content">
+              <h2>Your Pets Health</h2>
+              <p>
+                Can’t remember the last time your pet had their rabies
+                vaccination? How about the last time they went to the vet?{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card">
+            <img
+              className="card-image"
+              src="https://image.flaticon.com/icons/svg/87/87973.svg"
+              alt="clipboard icon"
+            />
+            <div className="card-content">
+              <h2>Save and Track</h2>
+              <p>
+                Pet.rx helps you organize and track your pet’s health
+                information in a convenient and accessible way so you can get
+                back to spending more of your time with your four-legged family
+                member.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card">
+            <img
+              className="card-image"
+              src="https://image.flaticon.com/icons/svg/88/88003.svg"
+              alt="pethouse"
+            />
+            <div className="card-content">
+              <h2>Let Pet.rx Work For You</h2>
+              <p>
+                With the ability to track vaccination records, weight, chronic
+                conditions, amongst other things across time you’ll never be
+                left wondering
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottombuttonsbox">
+        <button className="bottombuttons">Demo</button>
+        <button className="bottombuttons">Sign Up</button>
       </div>
     </div>
   );
--- src/components/Landing/navbar.js
+++ src/components/Landing/navbar.js
@@ -2,31 +2,30 @@ import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import "./navbar.css";
 
+export default class Navbar extends React.Component {
+  componentDidMount() {
+    // code for sticky navbar on landingpage
+    window.onscroll = myFunction;
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+  }
 
-export default function Navbar(props) {
+  render() {
+    return (
+      <div className="nav header" id="myHeader">
+        <div className="logo">Pet.rx</div>
 
-  // componentDidMount() {
-  //   // When the user scrolls the page, execute myFunction
-  //   window.onscroll = function() {myFunction()}
-  //   // Get the header
-  //   var header = document.getElementById("myHeader")
-  //   // Get the offset position of the navbar
-  //   var sticky = header.offsetTop
-  //   // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  //   function myFunction() {
-  //     if (window.pageYOffset > sticky) {
-  //       header.classList.add("sticky");
-  //     } else {
-  //       header.classList.remove("sticky");
-  //     }
-  //   };
-
-// }
-  return (
-
-    <div className="nav header" id="myHeader">
-    	<div className="logo">Pet.rx</div>
-      <button className="button"><Link to="/signup">Get Started</Link></button>
-    </div>
-  );
+        <Link to="/signup">
+          <button className="button">Get started!</button>
+        </Link>
+      </div>
+    );
+  }
 }
