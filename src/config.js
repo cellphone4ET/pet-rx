f405653afe4757099e6ce3f1f319eaf36e76fb88
Erica Johnson <ericacjohnson@gmail.com>
2018-10-06 17:21:22 -0700

    change variable name

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ require("dotenv").config();
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
 
-console.log(REACT_APP_API_BASE_URL);
+console.log(API_BASE_URL);
