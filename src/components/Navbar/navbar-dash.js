3d3d3b1f839080ca57df7984c50d14ef5c95f8e8
Erica Johnson <ericacjohnson@gmail.com>
2018-10-07 04:42:21 -0700

    clear auth token on click logout

--- src/components/Navbar/navbar-dash.js
+++ src/components/Navbar/navbar-dash.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { clearAuth } from "../../actions/auth";
 import "./navbar-dash.css";
+import store from "../../store";
 
-export default function NavbarDash(props) {
-  return (
-    <div className="nav2">
-      <Link to="/dashboard">
-        <div className="logo2">Pet.rx</div>
-      </Link>
-      <div className="links">
-        <ul>
-          <li>
-            <Link to="/addpet" className="linkz">
-              + add pet
-            </Link>
-          </li>
-          <li>
-            <Link to="/logout" className="linkz">
-              - logout
-            </Link>
-          </li>
-        </ul>
+export default class NavbarDash extends React.Component {
+  onClick() {
+    return store.dispatch(clearAuth());
+  }
+
+  render() {
+    return (
+      <div className="nav2">
+        <Link to="/dashboard">
+          <div className="logo2">Pet.rx</div>
+        </Link>
+        <div className="links">
+          <ul>
+            <li>
+              <Link to="/addpet" className="linkz">
+                + add pet
+              </Link>
+            </li>
+            <li>
+              <p className="linkz" onClick={() => this.onClick()}>
+                - logout
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
