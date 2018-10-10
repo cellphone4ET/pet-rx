087b663a6a5ebf34ebef95bf755692095f2a5556
Erica Johnson <ericacjohnson@gmail.com>
2018-10-09 17:08:12 -0700

    get all pets working

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -9,9 +9,8 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   if (action.type === actions.FETCH_PETS_SUCCESS) {
-    console.log(action);
     return Object.assign({}, state, {
-      pets: action.data.pets,
+      pets: action.data,
       error: null
     });
   } else if (action.type === actions.FETCH_ERROR) {
