a5a67be1dd99bf997498c3135266978a21b28514
Erica Johnson <ericacjohnson@gmail.com>
2018-10-15 14:57:31 -0700

    test

--- src/components/page-components/Navbar/navbar-login-signup.js
+++ src/components/page-components/Navbar/navbar-login-signup.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./navbar-login-signup.css";
 
 export default function NavbarLoginSignUp(props) {
+  //
   return (
     <div className="nav header1" id="myHeader">
       <Link to="/">
