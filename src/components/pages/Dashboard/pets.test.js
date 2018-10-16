8f82a2f265da7f79111a1d5aa896dcc8f62cb198
Erica Johnson <ericacjohnson@gmail.com>
2018-10-16 04:32:46 -0700

    pets component tests ppassing

--- src/components/pages/Dashboard/pets.test.js
+++ src/components/pages/Dashboard/pets.test.js
@@ -1,41 +1,39 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { connect } from "react-redux";
-
 import RequiresLogin from "../../requires-login";
 import { Pets } from "./pets";
 import { Pet } from "./pet";
-
 import { fetchProtectedData } from "../../../actions/protected-data";
 
-const mockFetchPetsAction = () => (dispatch, getState) => {};
-
+const mockFetchPetAction = {
+  type: "FETCH_PET"
+};
 jest.mock("../../../actions/protected-data", () =>
   Object.assign({}, require.requireActual("../../../actions/protected-data"), {
-    fetchPets: jest.fn().mockImplementation(() => {
-      return mockFetchPetsAction;
+    fetchProtectedData: jest.fn().mockImplementation(() => {
+      return mockFetchPetAction;
     })
   })
 );
 
-describe("<Pets />", () => {
+describe("<Pets/>", () => {
   let seedPets = [];
   beforeAll(() => {
     for (let i = 0; i < 10; i++) {
       seedPets.push({
-        name: "Pet Name"
+        pets: []
       });
     }
   });
 
   it("Renders without crashing", () => {
     const dispatch = jest.fn();
-    shallow(<Pets name="Happy" dispatch={dispatch} />);
+    shallow(<Pets pets={[]} dispatch={dispatch} />);
   });
 
-  // it("Dispatches mockFetchPetsAction on mount", () => {
-  //   const dispatch = jest.fn();
-  //   shallow(<Pets pets={seedPets} dispatch={dispatch} />);
-  //   expect(dispatch).toHaveBeenCalledWith(mockFetchPetsAction);
-  // });
+  it("Dispatches fetchPet on mount", () => {
+    const dispatch = jest.fn();
+    shallow(<Pets pets={[]} dispatch={dispatch} />);
+    expect(dispatch).toHaveBeenCalledWith(mockFetchPetAction);
+  });
 });
